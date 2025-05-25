b69c4287e35f901ea26e910d17f7e340b6786414
hopencode <newalfo1006@gmail.com>
2025-05-25 12:50:29 +0900

    Update ScheduleController.java

--- src/main/java/com/example/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/schedule/controller/ScheduleController.java
@@ -19,28 +19,31 @@ public class ScheduleController {
         this.scheduleService = scheduleService;
     }
 
-
+    // Lv1. 일정 생성(일정 작성하기)
     @PostMapping
     public ResponseEntity<ScheduleResponseDto> createSchedule(@RequestBody ScheduleRequestDto dto) {
 
         return new ResponseEntity<>(scheduleService.saveSchedule(dto), HttpStatus.CREATED);
     }
 
+    // Lv1 전체 일정 조회(등록된 일정 불러오기)
     @GetMapping
     public ResponseEntity<List<ScheduleResponseDto>> getSchedules(
-            @RequestParam(required = false) String email,
+            @RequestParam(required = false) String email,    // Lv3 연관 관계 설정 (email을 통해 동명이인 구분, schedule.sql에서 작성자 - 일정 연결)
             @RequestParam(required = false) String date
     ) {
 
         return new ResponseEntity<>(scheduleService.findAllSchedules(email, date), HttpStatus.OK);
     }
 
+    // Lv1 선택 일정 조회(선택한 일정 정보 불러오기)
     @GetMapping("/{id}")
     public ResponseEntity<ScheduleResponseDto> findScheduleById(@PathVariable Long id) {
 
         return new ResponseEntity<>(scheduleService.findScheduleById(id), HttpStatus.OK);
     }
 
+    // Lv2 선택한 일정 수정
     @PutMapping("/{id}")
     public ResponseEntity<ScheduleResponseDto> updateScheduleById(
             @PathVariable Long id,
@@ -50,6 +53,7 @@ public class ScheduleController {
         return new ResponseEntity<>(scheduleService.updateSchedule(id, dto.getTask(), dto.getPassword()), HttpStatus.OK);
     }
 
+    // Lv2 선택한 일정 삭제
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteScheduleById(
             @PathVariable Long id,
